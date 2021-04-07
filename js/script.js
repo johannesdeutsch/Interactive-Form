9e541a2e6ea2ea3100ded26a8e5ee3bd803b1b23
johannesdeutsch <johannes.deutsch@gmx.at>
2021-04-07 16:57:13 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -56,3 +56,79 @@ selectDesignElement.addEventListener('change', (event) => {
     }
 
 });
+
+// "Register for Activities" section
+const referenceFieldset = document.getElementById('activities');
+const referenceP = document.getElementById('activities-cost');
+console.log(referenceFieldset);
+console.log(referenceP);
+
+let totalCost = 0;
+
+referenceFieldset.addEventListener('change', (event) => {
+    const dataCost = +(event.target.getAttribute('data-cost'));
+    console.log(dataCost);
+    console.log(typeof dataCost);
+
+    if(event.target.checked) {
+        totalCost += dataCost;
+        referenceP.innerHTML = `Total: $${totalCost}`;
+    } else {
+        totalCost -= dataCost;
+        referenceP.innerHTML = `Total: $${totalCost}`;
+    }
+    console.log(totalCost);
+    console.log(event.target.checked);
+});
+
+// "Payment info" section
+const selectPayment = document.getElementById('payment');
+const divCreditCard = document.getElementById('credit-card');
+const divPaypal = document.getElementById('paypal');
+const divBitcoin = document.getElementById('bitcoin');
+
+console.log(selectPayment);
+console.log(divCreditCard);
+console.log(divPaypal);
+console.log(divBitcoin);
+
+//hide bitcoin and paypal methods
+divPaypal.hidden = true;
+divBitcoin.hidden = true;
+
+//target Payment select element's second child and give it the selected property
+const secondSelectChild = selectPayment.children[1];
+secondSelectChild.setAttribute('selected', 'selected');
+
+selectPayment.addEventListener('change', (event) => {
+    if (event.target.value === 'credit-card') {
+        divCreditCard.hidden = false;
+        divPaypal.hidden = true;
+        divBitcoin.hidden = true;
+    } else if(event.target.value === 'paypal') {
+        divCreditCard.hidden = true;
+        divPaypal.hidden = false;
+        divBitcoin.hidden = true;
+    } else if(event.target.value === 'bitcoin') {
+        divCreditCard.hidden = true;
+        divPaypal.hidden = true;
+        divBitcoin.hidden = false;
+    }
+});
+
+//Form Validation
+// select Elements
+const getEmailField = document.getElementById('email');
+const getCardNumberField = document.getElementById('cc-num');
+const getZipCodeField = document.getElementById('zip');
+const getCVVField = document.getElementById('cvv');
+const getFormElement = document.getElementsByTagName('form');
+
+getFormElement.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let nameElementValue = getNameField.value;
+    let testNameValue = /^[a-zA-Z]+ [a-zA-Z]*? [a-zA-Z]+?$/.test(nameElementValue);
+    
+    console.log(nameElementValue);
+    console.log(testNameValue);
+});
